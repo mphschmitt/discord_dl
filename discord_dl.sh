25e5b80de2e401396dda3cd5414eec4d5e97b362
Mathias Schmitt <mathiaspeterhorst@gmail.com>
2021-04-04 22:47:21 +0200

    [DEV] discord_dl: add options
    Add version, help, canary and ptg options.

--- discord_dl.sh
+++ discord_dl.sh
@@ -17,13 +17,32 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 PROGRAM_NAME="discord"
+VERSION="1.0.0"
 
 APP_DIRECTORY="/opt"
 APP_NAME="Discord"
 
 DOWNLOAD_DIRECTORY="/tmp"
-DOWNLOAD_PARAMETERS="stable?platform=linux&format=tar.gz"
-URL="https://discord.com/api/download/$DOWNLOAD_PARAMETERS"
+URL_PTB="https://discord.com/api/download?platform=linux&format=tar.gz"
+URL_CANARY="https://canary.discord.com/api/download?platform=linux&format=tar.gz"
+
+version () {
+	printf "discord_dl $VERSION\n"
+	printf "\n"
+	printf "Copyright (C) 2021 Mathias Schmitt\n"
+	printf "License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.\n"
+	printf "This is free software, and you are welcome to change and redistribute it\n"
+	printf "This program comes with ABSOLUTELY NO WARRANTY.\n"
+}
+
+usage () {
+	printf "Usage: discord_dl [OPTIONS]\n"
+	printf "Download and update discord for Linux\n"
+	printf "  -h  --help       display this help message and exit\n"
+	printf "  -p  --ptb        install the ptb version\n"
+	printf "  -c  --canary     install the canary version\n"
+	printf "  -v  --version    output version information and exit\n"
+}
 
 get_distribution () {
 	OS="UNKNOWN"
@@ -70,41 +89,72 @@ get_cmd () {
 	echo "$CMD"
 }
 
-if [[ $(command -v curl) == ""  ]]
-then
-	echo "curl not found, trying wget..."
+install () {
+	printf "Installing discord %b\n" $1
 
-	if [[ $(command -v wget) == "" ]]
+	if [[ $(command -v curl) == ""  ]]
 	then
-		echo "wget not found..."
-		DISTRIBUTION=$(get_distribution)
-		echo "Linux distribution => $DISTRIBUTION"
+		echo "curl not found, trying wget..."
 
-		PKG_MNGR=$(get_package_manager "$DISTRIBUTION")
-		echo "package manager => $PKG_MNGR"
-		if [ "$PKG_MNGR" == "UNKNOWN" ]
+		if [[ $(command -v wget) == "" ]]
 		then
-			echo "discord_dl could not detect your package manager."
-			echo "Please, install either curl or wget with your distribution's package manager, and relaunch discord_dl"
+			echo "wget not found..."
+			DISTRIBUTION=$(get_distribution)
+			echo "Linux distribution => $DISTRIBUTION"
+
+			PKG_MNGR=$(get_package_manager "$DISTRIBUTION")
+			echo "package manager => $PKG_MNGR"
+			if [ "$PKG_MNGR" == "UNKNOWN" ]
+			then
+				echo "discord_dl could not detect your package manager."
+				echo "Please, install either curl or wget with your distribution's package manager, and relaunch discord_dl"
+				exit
+			fi
+			CMD=$(get_cmd $PKG_MNGR)
+			echo "It seems curl and wget aren't installed on your system. Please, run '$CMD' and relaunch discord_dl"
 			exit
 		fi
-		CMD=$(get_cmd $PKG_MNGR)
-		echo "It seems curl and wget aren't installed on your system. Please, run '$CMD' and relaunch discord_dl" 
-		exit
+
+		wget "$1" -P "$DOWNLOAD_DIRECTORY"
+	else
+		curl -L "$1" -o "$DOWNLOAD_DIRECTORY/$APP_NAME.tar.gz"
 	fi
 
-	wget "$URL" -P "$DOWNLOAD_DIRECTORY"
-else
-	curl -L "$URL" -o "$DOWNLOAD_DIRECTORY/$APP_NAME.tar.gz"
-fi
+	tar -zxvf "$DOWNLOAD_DIRECTORY/$APP_NAME.tar.gz" -C /tmp
+	sudo rm -rf "$APP_DIRECTORY/$APP_NAME"
+	sudo mv "$DOWNLOAD_DIRECTORY/$APP_NAME" "$APP_DIRECTORY/$APP_NAME"
 
-tar -zxvf "/tmp/$APP_NAME.tar.gz" -C /tmp
-sudo rm -rf "$APP_DIRECTORY/$APP_NAME"
-sudo mv "$DOWNLOAD_DIRECTORY/$APP_NAME" "$APP_DIRECTORY/$APP_NAME"
+	if [ -f /usr/bin/$PROGRAM_NAME ]
+	then
+		sudo rm "/usr/bin/$PROGRAM_NAME"
+	fi
+	sudo ln -s "$APP_DIRECTORY/$APP_NAME/$APP_NAME" "/usr/bin/$PROGRAM_NAME"
+}
 
-if [ -f /usr/bin/$PROGRAM_NAME ]
+if [ $# -eq 0 ]
 then
-	sudo rm "/usr/bin/$PROGRAM_NAME"
+	install $URL_PTB
+elif [ $# -eq 1 ]
+then
+	if [ "$1" == "-v" ] || [ "$1" == "--version" ]
+	then
+		version
+		exit
+	elif [ "$1" == "-c" ] || [ "$1" == "--canary" ]
+	then
+		install $URL_CANARY
+	elif [ "$1" == "-p" ] || [ "$1" == "--ptb" ]
+	then
+		install $URL_PTB
+	elif [ "$1" == "-h" ] || [ "$1" == "--help" ]
+	then
+		usage
+	else
+		printf "Invalid argument: %b\n" "$1"
+		usage
+	fi
+else
+	printf "Too many arguments\n"
+	usage
 fi
-sudo ln -s "$APP_DIRECTORY/$APP_NAME/$APP_NAME" "/usr/bin/$PROGRAM_NAME"
 
